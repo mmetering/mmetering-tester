c8244251ec4a2f12e8cd84c3e299c0f65dfb263f
chrisonntag <sonntagc@fim.uni-passau.de>
2017-10-05 17:28:38 +0200

    Initial setup

---
+++ application/SDM630.py
@@ -0,0 +1,40 @@
+import minimalmodbus
+import datetime
+
+class Meter:
+    def __init__(self, id, address, port, start=None, end=None):
+        self.id = id
+        self.address = address
+        self.port = port
+        self.start = start
+        self.end = end
+        self.connected = False
+
+        # port name, slave address (in decimal)
+        self.instrument = minimalmodbus.Instrument(port, address)
+        self.instrument.debug = False
+        self.instrument.serial.timeout = 2.0 #sec
+        self.instrument.serial.baudrate = 19200 # Baud
+        self.instrument.serial.bytesize = 8
+        self.instrument.serial.parity   = minimalmodbus.serial.PARITY_NONE
+        self.instrument.serial.stopbits = 1
+        self.instrument.handle_local_echo = False
+
+    def is_reachable(self):
+        return self.getHoldingRegister('0x1E', 2) == 3.0
+
+    def getHoldingRegister(self, hexc, length):
+        return self.getRegister(hexc, 3, length)
+
+    def getInputRegister(self, hexc, length):
+        return self.getRegister(hexc, 4, length)
+
+    def getRegister(self, hexc, code, length):
+        try:
+            return self.instrument.read_float(int(hexc, 16), functioncode=code, numberOfRegisters=length)
+        except OSError:
+            pass
+        except RuntimeError:
+            print("There has been an runtime error", file=sys.stderr)
+            print("Exception: ", exc_info=True, file=sys.stderr)
+
---
+++ application/main.py
@@ -0,0 +1,91 @@
+import sys, glob, serial
+import tkinter as tk
+import tkinter.ttk as ttk
+import serial.tools.list_ports
+from redirectors import TextRedirector
+from SDM630 import Meter
+import threading
+
+
+class MeterTester(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.title("MMetering - Tester")
+        self.selected = tk.StringVar()
+
+        top_frame = tk.Frame(self, bd=2)
+        bottom_frame = tk.Frame(self, bd=2)
+        top_frame.pack(fill="x")
+        bottom_frame.pack(fill="x")
+
+        label = tk.Label(top_frame, text="Select Port: ")
+        label.pack(side="left")
+        self.cb = ttk.Combobox(top_frame, textvariable=self.selected, values=self.get_ports())
+        self.cb.pack(side="left", fill="x")
+
+        scrollbar = tk.Scrollbar(bottom_frame)
+        scrollbar.pack(side="right", fill="y")
+        self.text_box = tk.Text(bottom_frame, wrap = "word", borderwidth="1")
+        self.text_box.pack(side="bottom", fill="both", expand=True)
+
+        self.text_box.config(yscrollcommand=scrollbar.set)
+        scrollbar.config(command=self.text_box.yview)
+
+        self.stop_btn = tk.Button(top_frame, text='Stop', command=self.stop_thread)
+        self.stop_btn.pack(side="right")
+        self.start_btn = tk.Button(top_frame, text='Start', command=lambda: threading.Thread(target=self.check_meters).start())
+        self.start_btn.pack(side="right")
+
+        sys.stdout = TextRedirector(self.text_box, "stdout")
+        sys.stderr = TextRedirector(self.text_box, "stderr")
+
+        self.exit_thread = False
+
+    def stop_thread(self):
+        self.exit_thread = True
+
+    def get_ports(self):
+        """ Lists serial port names
+
+            :raises EnvironmentError:
+                On unsupported or unknown platforms
+            :returns:
+                A list of the serial ports available on the system
+        """
+        if sys.platform.startswith('win'):
+            ports = ['COM%s' % (i + 1) for i in range(256)]
+        elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+            # this excludes your current terminal "/dev/tty"
+            ports = glob.glob('/dev/tty[A-Za-z]*')
+        elif sys.platform.startswith('darwin'):
+            ports = glob.glob('/dev/tty.*')
+        else:
+            raise EnvironmentError('Unsupported platform')
+
+        result = []
+        for port in ports:
+            try:
+                s = serial.Serial(port)
+                s.close()
+                result.append(port)
+            except (OSError, serial.SerialException):
+                pass
+        return result
+
+    def check_meters(self):
+        print("Testing Meters 1-36\nPort: %s\n\n" % self.selected.get())
+        while not self.exit_thread:
+            for i in range(1, 37):
+                meter = Meter(i, i, self.selected.get())
+                if i == 1:
+                    print(meter.instrument)
+                if meter.is_reachable():
+                    print("✅ Meter %d is connected" % i)
+                else:
+                    print("❌ Can't reach Meter %d" % i)
+
+
+if __name__ == '__main__':
+    app = MeterTester()
+    app.mainloop()
+
